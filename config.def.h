f15ff03c41eb99b429e4b3f7fec72a2ae85fa2d3
Lorenzo Torres <lolztorres@protonmail.com>
2023-04-02 20:46:55 +0200

    feat: added theme

--- config.def.h
+++ config.def.h
@@ -7,15 +7,16 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#504945";
+static const char col_gray3[]       = "#bdae93";
+static const char col_gray4[]       = "#ebdbb2";
+static const char col_cyan[]        = "#cc241d";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_gray2,  col_gray1  },
 };
 
 /* tagging */
@@ -61,8 +62,10 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *incrvol[] = { "mixer", "vol", "+1:+1", NULL};
 static const char *decrvol[] = { "mixer", "vol", "-1:-1", NULL};
+static const char *screenshot[] = { "maim", "-s", "|", "xclip", "-selection", "clipboard", "-t", "image/png", NULL};
 
 #define XK_XF86AudioLowerVolume 0x1008ff11
+#define XK_PrintScreen	        0x0000ff61
 #define XK_XF86AudioRaiseVolume 0x1008ff13
 
 static const Key keys[] = {
@@ -102,6 +105,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ 0x0,                          XK_XF86AudioRaiseVolume,    spawn,           {.v = incrvol } },
 	{ 0x0,                          XK_XF86AudioLowerVolume,    spawn,           {.v = decrvol } },
+	{ 0x0,				XK_PrintScreen,		    spawn,	     {.v = screenshot } },
 };
 
 /* button definitions */
